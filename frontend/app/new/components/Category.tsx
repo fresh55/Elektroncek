5db79fdf9e2c5464502461a6ed42937545d952ad
Bruno <bruno.gale97@gmail.com>
2023-12-31 11:13:37 +0100

    category update

--- frontend/app/new/components/Category.tsx
+++ frontend/app/new/components/Category.tsx
@@ -6,63 +6,95 @@ import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import { Controller, useForm } from "react-hook-form";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
-import { AlertTriangle, Hourglass, Laugh, Sparkles } from "lucide-react";
+import {
+  AlertTriangle,
+  Hourglass,
+  Laugh,
+  Sparkles,
+  ChevronLeft,
+  StepForward,
+} from "lucide-react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
 import Image from "next/image";
-import { motion,AnimatePresence  } from 'framer-motion';
-import { useEffect, useState } from 'react';
+import { motion, AnimatePresence } from "framer-motion";
+import { useEffect, useState } from "react";
 
-import { categories,Category, Subcategory } from './categories';
+import { categories, Category } from "./categories";
+import React from "react";
 const formSchema = z.object({
   ime: z.string().min(1, "Naslov produkta je obvezen ! "),
   status: z.enum(["novo", "kot novo", "rabljeno", "poškodovano"]),
   cena: z.string(),
 });
 
-
+type BreadcrumbsProps = {
+  steps: Category[];
+};
 export const SomeComponent2 = () => {
   const { items, currentStep, goNext, goPrev } = useStep();
-  const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
-  const [selectedSubcategory, setSelectedSubcategory] = useState<string | null>(null);
-  const [isVisible, setIsVisible] = useState(true);
+  const [breadcrumbs, setBreadcrumbs] = useState<Category[]>([]);
+  const [currentSelection, setCurrentSelection] =
+    useState<Category[]>(categories);
+
+    const handleCategoryClick = (category: Category) => {
+      // If there are subcategories, allow further navigation
+      if (category.subcategories && category.subcategories.length > 0) {
+        setBreadcrumbs([...breadcrumbs, category]);
+        setCurrentSelection(category.subcategories);
+      } else {
+        // If no subcategories, don't update breadcrumbs or currentSelection
+        // This will prevent further navigation
+      }
+    };
+  
+    // Determine if the end of subcategories is reached
+    const isEndOfSubcategories = currentSelection.length === 0;
+  
+    // ... [rest of the existing code]
+
+  const goBack = () => {
+    const newBreadcrumbs = breadcrumbs.slice(0, -1);
+    setBreadcrumbs(newBreadcrumbs);
+    const parentCategory = newBreadcrumbs[newBreadcrumbs.length - 1] || null;
+    setCurrentSelection(
+      parentCategory ? parentCategory.subcategories || [] : categories
+    );
+  };
+
+
+  const Breadcrumbs: React.FC<BreadcrumbsProps> = ({ steps }) => (
+    <div className="flex text-center rounded-full border">
+      {steps.map((step, index) => (
+        <React.Fragment key={step.id}>
+          {index > 0 && <span className="text-xl">{">"}</span>}
+          <button className="bg-ghost  inline-flex items-center  px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 " onClick={() => {/* handle breadcrumb click */}}>
+            {step.title} {/* Display the title or any other string property */}
+          </button>
+        </React.Fragment>
+      ))}
+    </div>
+  );
+
   const variants = {
-    hidden: { 
-      opacity: 0, 
+    hidden: {
+      opacity: 0,
       scale: 0, // more dramatic shrinking
-      y: 50 // move down on exit
+      y: 50, // move down on exit
     },
-    visible: { 
-      opacity: 1, 
-      scale: 1, 
-      y: 0 // return to original position
+    visible: {
+      opacity: 1,
+      scale: 1,
+      y: 0, // return to original position
     },
   };
-  
+
   const clickTransition = {
     duration: 0.8, // longer duration for a more noticeable effect
     type: "spring",
     stiffness: 100, // adjust stiffness for spring animation
   };
 
-  const handleCategoryClick = (categoryValue: string) => {
-    setIsVisible(false);
-    setSelectedCategory(categoryValue);
-    setSelectedSubcategory(null); // Reset subcategory selection when a category is clicked
-  };
-  
-  const handleSubcategoryClick = (subcategoryValue: string) => {
-    setIsVisible(true);
-    setSelectedSubcategory(subcategoryValue);
-  };
-  useEffect(() => {
-    if (selectedCategory !== null || selectedCategory === null && !isVisible) {
-      setIsVisible(true);
-    }
-  }, [selectedCategory, isVisible]);
-
-  const selectedCategoryData = categories.find(category => category.value === selectedCategory);
- 
   const {
     handleSubmit,
     register,
@@ -87,102 +119,79 @@ export const SomeComponent2 = () => {
           </Badge>
           <CardTitle className="text-[24px]">{currentStep.title}</CardTitle>
         </CardHeader>
+        <div className="ml-4">
+          {breadcrumbs.length > 0 && (
+            <> <div className="flex justify-center"><Breadcrumbs steps={breadcrumbs} /></div>
+              
+              <Button onClick={goBack}>
+                <ChevronLeft />
+                Vrni se
+              </Button>
+            </>
+          )}
+        </div>
+        
 
         <Separator className="mb-8" />
 
         <CardContent>
           <div className="mb-2">
-          <Controller
-  name="status"
-  control={control} // from useForm()
-  render={({ field }) => (
-    <AnimatePresence>
+            <Controller
+              name="status"
+              control={control} // from useForm()
+              render={({ field }) => (
+                <AnimatePresence>
                   <RadioGroup
                     onValueChange={field.onChange}
                     className="grid grid-cols-4 gap-4"
                   >
-                    {selectedCategory === null ? (
-                      categories.map((category: Category) => (
-                        <motion.div
-                          key={category.id}
-                          variants={variants}
-                          layout
-                          initial={{ opacity: 0 }}
-                          animate={{ opacity: 1 }}
-                          exit={{ opacity: 0 }}
-                          transition={{ duration: 0.4, type: "spring", bounce: 0.25 }}
-                          onClick={() => handleCategoryClick(category.value)}
+                    {currentSelection.map((category) => (
+                      <motion.div
+                        key={category.id}
+                        variants={variants}
+                        layout
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
+                        transition={{
+                          duration: 0.4,
+                          type: "spring",
+                          bounce: 0.25,
+                        }}
+                        onClick={() => handleCategoryClick(category)}
+                      >
+                        <RadioGroupItem
+                          value={category.value}
+                          id={category.id}
+                          className="peer sr-only "
+                        />
+
+                        <Label
+                          htmlFor={category.id}
+                         
+                          
+
+
+                          
                         >
-          <RadioGroupItem
-            value={category.value}
-            id={category.id}
-            className="peer sr-only"
-          />
-
-          <Label
-            htmlFor={category.id}
-            className="flex flex-col cursor-pointer items-center text-center p-4 h-42 rounded-md border border-muted bg-popover hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:bg-muted  [&:has([data-state=checked])]:border-primary"
-          >
-            <Image
-              className="rounded-sm mb-2"
-              src={category.imageSrc}
-              width={100}
-              height={100}
-              alt={category.title}
+                          <Image
+                            className="rounded-sm mb-2"
+                            src={category.imageSrc}
+                            width={112}
+                            height={112}
+                            alt={category.title}
+                          />
+                          <h3 className="text-lg">{category.title}</h3>
+                          <span className="text-xs opacity-70 text-zinc-900 mt-2 block">
+                            {category.description}
+                          </span>
+                        </Label>
+                      </motion.div>
+                    ))}
+                  </RadioGroup>
+                </AnimatePresence>
+              )}
             />
-            <h3 className="text-lg">{category.title}</h3>
-            <span className="text-xs opacity-70 text-zinc-900 mt-2 block">
-              {category.description}
-            </span>
-          </Label>
-          </motion.div>
-    ))
-  ) : (
-    
-         
-        selectedCategoryData?.subcategories?.map((sub: Subcategory) => (
-          <motion.div
-          key={sub.id}
-          variants={variants}
-          layout
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-          transition={{ duration: 0.4, type: "spring", bounce: 0.25 }}
-          onClick={() => handleCategoryClick(sub.value)}
-        >
-<RadioGroupItem
-value={sub.value}
-id={sub.id}
-className="peer sr-only"
-/>
-
-<Label
-htmlFor={sub.id}
-className="flex flex-col cursor-pointer items-center text-center p-4 h-42 rounded-md border border-muted bg-popover hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:bg-muted  [&:has([data-state=checked])]:border-primary"
->
-<Image
-className="rounded-sm mb-2"
-src={sub.imageSrc}
-width={100}
-height={100}
-alt={sub.title}
-/>
-<h3 className="text-lg">{sub.title}</h3>
-<span className="text-xs opacity-70 text-zinc-900 mt-2 block">
-{sub.description}
-</span>
-</Label>
-</motion.div>
-        ))
-      
- 
-  )}
-      
-    </RadioGroup>
-    </AnimatePresence>
-  )}
-/>
             <span className="text-xs opacity-70 text-zinc-900 mt-2">
               Čim bolj natančno opišite stanje artikla, ki ga ponujate, in vse
               pomembne podrobnosti pokažite na slikah artikla. To vam prihrani
--- frontend/app/new/components/categories.tsx
+++ frontend/app/new/components/categories.tsx
@@ -1,76 +1,75 @@
 export type Category = {
   value: string;
   id: string;
-  imageSrc: string;
+  imageSrc?: string;
   title: string;
   description: string;
-  subcategories?: Subcategory[];
+  subcategories?: Category[]; // Subcategories are also of type Category
 };
-export type Subcategory = {
-  value: string;
-  id: string;
-  title: string;
-  description?: string;
-  imageSrc?: string;
-  // Add any additional properties needed for subcategories
-};
-
 
 export const categories: Category[] = [
   {
-    value: "racunalnistvo",
+    value: "Računalništvo",
     id: "racunalnistvo",
-    imageSrc: "/images/note.png",
+    imageSrc: "/images/nzxt.png",
     title: "Računalništvo",
     description:
       "Ta kategorija zajema vse vrste računalnikov ter njihovo pripadajočo periferno opremo in komponente.",
     subcategories: [
       {
         id: "prenosni",
-        value: "prenosni",
+        value: "Prenosni računalniki",
         title: "Prenosni računalniki",
+        imageSrc: "/images/prenosnik1.png",
         description: "A variety of laptops...",
       },
       {
         id: "namizni",
         value: "namizni",
         title: "Namizni računalniki",
+        imageSrc: "/images/nzxt.png",
         description: "A variety of laptops...",
       },
       {
         id: "tablice",
         value: "tablice",
         title: "Tablice & dodatki",
+        imageSrc: "/images/tablica.png",
         description: "A variety of laptops...",
       },
       {
         id: "monitorji",
         value: "monitorji",
         title: "Monitorji",
+        imageSrc: "/images/monitor.png",
         description: "A variety of laptops...",
       },
       {
         id: "tiskalniki",
         value: "tiskalniki",
         title: "Tiskalniki in 3D tiskalniki",
+        imageSrc: "/images/printer2.png",
         description: "A variety of laptops...",
       },
       {
         id: "ereader",
         value: "ereader",
         title: "E-bralniki in Grafične tablice",
+        imageSrc: "/images/graphic1.png",
         description: "A variety of laptops...",
       },
       {
         id: "racdodatki",
         value: "racdodatki",
         title: "Računalniški dodatki",
+        imageSrc: "/images/keyboard.png",
         description: "A variety of laptops...",
       },
       {
         id: "komponente",
         value: "komponente",
         title: "Računalniške komponente",
+        imageSrc: "/images/gpu.png",
         description: "A variety of laptops...",
       },
     ],
@@ -78,16 +77,35 @@ export const categories: Category[] = [
   {
     value: "telefonija",
     id: "telefonija",
-    imageSrc: "/images/phon.png",
+    imageSrc: "/images/phone3.png",
     title: "Telefonija",
     description:
       "V kategoriji telefonija najdemo mobilne naprave, ter pripadajočo dodatno opremo, ki obsega polnilce, ovitke in slušalke.",
+      subcategories: [
+        {
+          id: "prenosni",
+          value: "prenosni",
+          title: "Prenosni računalniki",
+          description: "A variety of laptops...",
+          subcategories: [{
+            id: "prenosni123",
+            value: "prenosni123",
+            title: "Prenosni računalniki1",
+            description: "A variety of laptops..."},
+            {
+              id: "prenosni1234",
+              value: "prenosni1234",
+              title: "Prenosni računalniki2",
+              description: "A variety of laptops..."}
+          ],
+        },
+      ],
   },
 
   {
     value: "televizija",
     id: "televizija",
-    imageSrc: "/images/tv2.png",
+    imageSrc: "/images/tvsamsung.png",
     title: "Televizija in video",
     description:
       "Kategorija TV in Video vključuje raznolike televizorje, od pametnih do visokoločljivostnih modelov, pa tudi naprave za predvajanje in snemanje.",
@@ -95,7 +113,7 @@ export const categories: Category[] = [
   {
     value: "audio",
     id: "audio",
-    imageSrc: "/images/headphones1.png",
+    imageSrc: "/images/headphones3.png",
     title: "Audio in glasbila",
     description:
       "Kategorija TV in Video vključuje raznolike televizorje, od pametnih do visokoločljivostnih modelov, pa tudi naprave za predvajanje in snemanje.",
@@ -103,7 +121,7 @@ export const categories: Category[] = [
   {
     value: "gaming",
     id: "gaming",
-    imageSrc: "/images/gaming.png",
+    imageSrc: "/images/ps5.png",
     title: "Gaming in VR",
     description:
       "Kategorija TV in Video vključuje raznolike televizorje, od pametnih do visokoločljivostnih modelov, pa tudi naprave za predvajanje in snemanje.",
